1cf66fb197f2e11a717c9a9acb790b5efdfca179
Davi Garcia <davigs@hotmail.com>
2021-07-09 16:00:50 -0300

    add footer component basic structure

--- corebiz/src/components/Footer/index.js
+++ corebiz/src/components/Footer/index.js
@@ -3,7 +3,23 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      FOOTER
+      <div>
+        <h4>Localização</h4>
+        <p>
+          Avenida Andrômeda, 2000. Bloco 6 e 8 <br/>
+          Alphavile SP <br/>
+          brasil@corebiz.ag <br/>
+          +55 11 3090 1039
+        </p>
+      </div>
+      <div>
+        <button>ENTRE EM CONTATO</button>
+        <button>FALE COM O NOSSO <br/> CONSULTOR ONLINE</button>
+      </div>
+      <div>
+        <span>codebiz</span>
+        <span>vtex</span>
+      </div>
     </div>
   )
 }

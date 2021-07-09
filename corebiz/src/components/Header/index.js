bccdddabe0ef970388cda56338b79d381ce0f52e
Davi Garcia <davigs@hotmail.com>
2021-07-09 15:03:31 -0300

    add header component

--- corebiz/src/components/Header/index.js
+++ corebiz/src/components/Header/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {ReactComponent as Logo} from "../../assets/logo.svg"
+import {ReactComponent as SearchIcon} from "../../assets/search.svg"
+import {ReactComponent as UserIcon} from "../../assets/user.svg"
+import {ReactComponent as CartIcon} from "../../assets/cart.svg"
+import * as S from "./styled"
 
-export default function Header() {
+export default function Header({amount}) {
   return (
-    <div>
-      HEADER
-    </div>
+    <S.Header>
+      <Logo/>
+      <S.MenuWrapper>
+        <S.SearchWrapper>
+          <S.SearchInput type="text" placeholder="O que está procurando?" />
+          <SearchIcon className="search-icon"/>
+        </S.SearchWrapper>
+        <S.UserWrapper>
+          <UserIcon/>
+          <span>Minha Conta</span>
+        </S.UserWrapper>
+        <S.CartWrapper>
+          <CartIcon/>
+          <span>{amount}</span>
+        </S.CartWrapper>
+      </S.MenuWrapper>
+    </S.Header>
   )
 }

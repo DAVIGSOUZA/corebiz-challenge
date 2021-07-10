a02fe5c8b552c3ca22b2ef2e763f4cebc2fd3ef1
Davi Garcia <davigs@hotmail.com>
2021-07-09 23:26:39 -0300

    add behavior to  newsletter component

--- corebiz/src/components/NewsletterForm/index.js
+++ corebiz/src/components/NewsletterForm/index.js
@@ -1,29 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { emailRegex, BASE_URL } from "../../utils";
+import axios from "axios";
 
 export default function NewsletterForm () {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [errors, setErrors] = useState({})
+  const [showForm, setShowForm] = useState(true)
+
+
+  const validateForm = () => {
+    const error = { name: '', email: '' }
+
+    name.length > 3 
+      ? error.name = ''
+      : error.name = 'Preencha com seu nome completo'
+
+    emailRegex.test(email) 
+      ? error.email = ''
+      : error.email = 'Preencha com um e-mail válido'
+
+    setErrors(error)
+
+    return error.name?.length || error.email?.length ? true : false
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+
+    if (validateForm()) return
+
+    const data = { name, email }
+
+    axios.post(`${BASE_URL}newsletter`, data)
+      .then(res => setShowForm(false))
+      .catch(err => console.log(err)) 
+
+    setName('')
+    setEmail('')
+  }
+  
   return (
-    <div>
-      <div>
-        <h3>
-          Participe de nossas news com promoções e novidades!
-        </h3>
-        <form>
-          <div>
-            <input type="text" placeholder="Digite seu nome"/>
-            <span> erro </span>
-          </div>
-          <div>
-            <input type="text" placeholder="Digite seu email"/>
-            <span> erro </span>
-          </div>
-          <button>Eu quero!</button>
-        </form>
-      </div>
-      <div>
-        <h3>Seu e-mail foi cadastrado com sucesso!</h3>
-        <h4>A partir de agora você receberá as novidades e ofertas exclusivas.</h4>
-        <button>Cadastrar novo e-mail</button>
-      </div>
-    </div>
+    <>
+      {showForm ? (
+        <div>
+          <h3>
+            Participe de nossas news com promoções e novidades!
+          </h3>
+          <form onSubmit={submit}>
+            <div>
+              <input 
+                type="text" 
+                placeholder="Digite seu nome" 
+                onChange={e => setName(e.target.value)}
+              />
+              <span> {errors.name} </span>
+            </div>
+            <div>
+              <input 
+                type="email" 
+                placeholder="Digite seu email" 
+                onChange={e => setEmail(e.target.value)}
+              />
+              <span> {errors.email} </span>
+            </div>
+            <button type="submit">Eu quero!</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h3>Seu e-mail foi cadastrado com sucesso!</h3>
+          <h4>A partir de agora você receberá as novidades e ofertas exclusivas.</h4>
+          <button onClick={() => setShowForm(true)}>Cadastrar novo e-mail</button>
+        </div>        
+      )}
+    </>
   )
 }
---
+++ corebiz/src/utils/index.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://corebiz-test.herokuapp.com/api/v1/'
+
+export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/

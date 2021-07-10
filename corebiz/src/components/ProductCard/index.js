de6b0d9a40a9a321c2ec31737e7dafcae5cc4cfa
Davi Garcia <davigs@hotmail.com>
2021-07-10 01:28:18 -0300

    add basic structure to product card and wrapper

--- corebiz/src/components/ProductCard/index.js
+++ corebiz/src/components/ProductCard/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { ReactComponent as StarSolid } from "../../assets/star-solid.svg";
+import { ReactComponent as StarOutlined } from "../../assets/star-outlined.svg";
 
-export default function ProductCard() {
+export default function ProductCard({product}) {
   return (
     <div>
-      PRODUCT CARD
+      <div>
+        <img src="" alt="produto" />
+        {/* {product.listPrice ? <div>offer tag</div> : null} */}
+      </div>
+      <div>
+        <h2>nome produto</h2>
+        <p> estrelas </p>
+        <p> preço original</p>
+        <p> preço </p>
+        <p> parcelamento </p>
+      </div>
+      <button> COMPRAR </button>
     </div>
   )
 }
--- corebiz/src/components/ProductsContainer/index.js
+++ corebiz/src/components/ProductsContainer/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import ProductCard from "../ProductCard";
+import ProductCard from "../ProductCard"
+import { ReactComponent as ArrowLeft } from "../../assets/arrowLeft.svg"
+import { ReactComponent as ArrowRight } from "../../assets/arrowRight.svg"
 
-export default function ProductsContainer() {
+export default function ProductsContainer({products}) {
+
   return (
     <div>
-      <ProductCard/>
-      <ProductCard/>
-      <ProductCard/>
+      <div>
+        <p> Mais vendidos </p>
+        <div>divider</div>
+      </div> 
+      <div>
+        <ArrowLeft/>
+        <div>
+          <ProductCard/>
+        </div>
+        <ArrowRight/>
+      </div>
     </div>
   )
 }
